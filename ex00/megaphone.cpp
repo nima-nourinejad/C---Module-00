d9d36864ed10203cfb035baca7a719ce9664a434
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-08-28 12:29:22 +0300

    updated ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,6 +2,28 @@
 #include <iostream>
 #include <string>
 
+std::string removeLeadingSpace(std::string str)
+{
+	unsigned int index = 0;
+
+	while(index < str.length() && str[index] == ' ')
+		index++;
+	if (index == str.length())
+		return ("");
+	return (str.substr(index));
+}
+
+std::string removeTrailingSpace(std::string str)
+{
+	int index = str.length() - 1;
+
+	while(index >= 0 && str[index] == ' ')
+		index--;
+	if (index == -1)
+		return ("");
+	return (str.substr(0, index + 1));
+}
+
 int main(int argc, char **argv) {
   if (argc < 2)
     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
@@ -9,12 +31,15 @@ int main(int argc, char **argv) {
     for (int index = 1; index < argc; index++) {
       std::string str = argv[index];
       int len = static_cast<int>(str.length());
+	  str = removeLeadingSpace(str);
+	  str = removeTrailingSpace(str);
       for (int i = 0; i < len; i++) {
         char c = static_cast<unsigned char>(str[i]);
         c = std::toupper(c);
         std::cout << c;
       }
-      std::cout << " ";
+	  if (index < argc -1)
+        std::cout << " ";
     }
   }
   std::cout << std::endl;
