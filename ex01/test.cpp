cdd970da9ef6a80ed7e135b085442de3ae97ff14
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-09-05 12:29:39 +0300

    now test accepts the number of contacts as argv

--- ex01/test.cpp
+++ ex01/test.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <string>
 
-int main(void) {
-  for (int i = 1; i <= 10; i++)
-  {
-	std::cout << "ADD" << std::endl;
-	std::cout << "Contact " << i << std::endl;
-	std::cout << "Contact " << i << std::endl;
-	std::cout << "Contact " << i << std::endl;
-	std::cout << i << std::endl;
-	std::cout << "Contact " << i << std::endl;
-  }
-  std::cout << "SEARCH" << std::endl;
-  return (0);
+int main(int argc, char **argv) {
+	int nbContacts;
+	if (argc < 2)
+		nbContacts = 10;
+	else
+		nbContacts = std::stoi(argv[1]);
+	for (int i = 1; i <= nbContacts; i++)
+	{
+		std::cout << "ADD" << std::endl;
+		std::cout << "Contact " << i << std::endl;
+		std::cout << "Contact " << i << std::endl;
+		std::cout << "Contact " << i << std::endl;
+		std::cout << i << std::endl;
+		std::cout << "Contact " << i << std::endl;
+	}
+	std::cout << "SEARCH" << std::endl;
+	return (0);
 }
