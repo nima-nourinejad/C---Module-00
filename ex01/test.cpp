78223e53da151b7f9fe01957c78ef62f61bce9ec
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-09-05 12:19:01 +0300

    added a test

---
+++ ex01/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void) {
+  for (int i = 1; i <= 10; i++)
+  {
+	std::cout << "ADD" << std::endl;
+	std::cout << "Contact " << i << std::endl;
+	std::cout << "Contact " << i << std::endl;
+	std::cout << "Contact " << i << std::endl;
+	std::cout << i << std::endl;
+	std::cout << "Contact " << i << std::endl;
+  }
+  std::cout << "SEARCH" << std::endl;
+  return (0);
+}
