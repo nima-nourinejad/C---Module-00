9c6a4382d08a7ae6e8fcb1f781a0acf17cb3159e
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-09-05 15:36:32 +0300

    update

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -43,26 +43,32 @@ std::string getTime() {
   /// Get the current timestamp
   time_t currentTimestamp;
   currentTimestamp = time(NULL);
+
   /// Convert the timestamp to a local time
   tm *localTime;
   localTime = localtime(&currentTimestamp);
+
   /// Get the year, month, day, hour, min, sec
   int year, month, day, hour, min, sec;
   grabData(localTime, year, month, day, hour, min, sec);
+
   /// Create a string stream to store the local time in the format of year_month_day_hour_min_sec
   std::stringstream stringStream;
   addData(stringStream, year, month, day, hour, min, sec);
 
   return stringStream.str();
 }
+
 ////////////////////////////////////////////////////// Display the current time required by header
 void _displayTimestamp(void) { std::cout << getTime() << std::endl; }
+
 ////////////////////////////////////////////////////// Create a log file name based on the current time and with a .log extension
 std::string logFileNameCreator(void) {
   std::stringstream stingStream;
   stingStream << getTime() << ".log";
   return stingStream.str();
 }
+
 ////////////////////////////////////////////////////// Create or append to a log file with time and with the message passed as an argument
 void logger(std::string message) {
   std::string logFileNameString = logFileNameCreator();
@@ -72,6 +78,7 @@ void logger(std::string message) {
           << " " << message << std::endl;
   logFile.close();
 }
+
 ////////////////////////////////////////////////////// Constructor with initial deposit and log the creation of the account
 Account::Account(int initial_deposit)
     : _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0),
@@ -83,6 +90,7 @@ Account::Account(int initial_deposit)
                << ";created";
   logger(stringStream.str());
 }
+
 ////////////////////////////////////////////////////// Destructor and log the closure of the account
 Account::~Account() {
   std::stringstream stringStream;
@@ -91,6 +99,7 @@ Account::~Account() {
   logger(stringStream.str());
 }
 
+////////////////////////////////////////////////////// Get the total number of accounts, total amount, total number of deposits, and total number of withdrawals (static)
 int Account::getNbAccounts(void) { return Account::_nbAccounts; }
 
 int Account::getTotalAmount(void) { return Account::_totalAmount; }
@@ -98,7 +107,8 @@ int Account::getTotalAmount(void) { return Account::_totalAmount; }
 int Account::getNbDeposits(void) { return Account::_totalNbDeposits; }
 
 int Account::getNbWithdrawals(void) { return Account::_totalNbWithdrawals; }
-////////////////////////////////////////////////////// Display the total number of accounts, total amount, total number of deposits, and total number of withdrawals
+
+////////////////////////////////////////////////////// Display the total number of accounts, total amount, total number of deposits, and total number of withdrawals (static)
 void Account::displayAccountsInfos(void) {
   std::stringstream stringStream;
   stringStream << "accounts:" << Account::getNbAccounts()
@@ -107,6 +117,7 @@ void Account::displayAccountsInfos(void) {
                << ";withdrawals:" << Account::getNbWithdrawals();
   logger(stringStream.str());
 }
+
 ////////////////////////////////////////////////////// Display the single account index, amount, number of deposits, and number of withdrawals
 void Account::displayStatus(void) const {
   std::stringstream stringStream;
@@ -148,9 +159,10 @@ bool Account::makeWithdrawal(int withdrawal) {
   logger(stringStream.str());
   return true;
 }
-
+////////////////////////////////////////////////////// Check the amount of the account required by the header
 int Account::checkAmount(void) const { return _amount; }
 
+////////////////////////////////////////////////////// Default constructor and log the creation of the account
 Account::Account()
     : _accountIndex(_nbAccounts), _amount(0), _nbDeposits(0),
       _nbWithdrawals(0) {
